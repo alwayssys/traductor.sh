ef2dd62089b341713aad4ba6250615b1ed08079e
alwayssys <143897924+alwayssys@users.noreply.github.com>
2023-09-05 19:25:27 -0600

    Add files via upload

---
+++ traductor.sh
@@ -0,0 +1,92 @@
+Happiness : La felicidad
+is a feeling : es un sentimiento
+or : o
+state : estado
+of : de
+mind : mente
+that : estaria
+a : un
+conscious : concentra
+being : llega
+experiences : experiencias
+when it reaches : cuando tiene
+moment : momento
+conformation : conformación
+well-being : bienestar
+certain : centra
+desirable : ciertos
+objectives : objetivos
+conscious : concentra
+individual have been : su propio
+individual have been achieved : su propio concepto
+It is considered that this state of mind : Se considera que este estado de ánimo
+and the factors : y los factores
+that condition : que los condicionan
+it are subjective : son subjetivos
+so each : por lo que cada
+person : persona
+can have : puede tener
+their own : su propio
+concept : concepto
+of happiness : de felicidad
+this differentiates : esto diferencia a
+from other : de otro
+sensations : sensaciones
+such as : como el
+cold : frio
+hunger : hambre
+which : para las que existe
+there is some : cierta
+of being more alike : por lo que parece mas parecido
+alike : acercarc
+individuals: individuos
+
+￼
+The happy face ,
+as : tal como
+shown by this smiley ,
+is a well-known symbol of : es un simbolo muy parecido a
+Some psychologists
+have tried : han tratado
+to characterize
+the degree : el grado
+of happiness
+through : mediante
+various : varios
+texts : textos
+and : y
+have come to define : y han llegado a definir
+happiness
+as a measure of : como una medida
+subjective : subjetivo
+self-perceived : auto percivido
+well-being that influences : que influye
+the attitudes : las actitudes
+and behavior :y el comportamiento
+of individuals : de los individuos
+People who have : las personas que demuestran
+a high degree : grado
+generally show : muestran generalmente
+positive environment approach : enfoque en el medio positivo
+at the same time : al mismo tiempo
+that : que
+they : se
+they feel : sienten
+conquer : conquistar
+contrary : al contrario
+to people who do not feel : que no sienten
+any : ningun
+degree : grado
+who show a negative environment approach : medio negativo
+feeling : sintiendose
+their : su
+
+It is understood in this context : se entiende en este contexto
+as a positive state of mind : como un estado de ánimo positivo
+said state of mind is subjective : dicho estado de ánimo es subjetivo
+therefore : por tanto
+refers to a self-perceived fact : se entiende como.un acto autopercibido
+This implies : esto implica
+that the same series of events : que una serie de eventos
+can be perceived differently by people : sea percivido dee manera diferente por personas
+with different temperaments : con diferente temperamento
